3f78fbf61f02b79333128a2b12d611591a15597c
Andreas Copan <avcopan@gmail.com>
2023-09-02 16:20:50 -0400

    Starter client using Vite

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import logoSvg from "/painless-panes-logo.svg";
+
+export default function App() {
+  return (
+    <>
+      <img src={logoSvg} className="logo" alt="Logo" />
+      <h1>Painless Panes</h1>
+    </>
+  );
+}
