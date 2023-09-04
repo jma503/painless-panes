7e06e88df1512097c15f62f5599e4d452c4af281
Andreas Copan <avcopan@gmail.com>
2023-09-04 13:18:03 -0400

    Form Page for email entry

--- src/subpages/EmailSubpage.jsx
+++ src/subpages/EmailSubpage.jsx
@@ -1,7 +1,38 @@
-export default function EmailSubpage() {
+import { useState } from "react";
+
+export default function FormEmailSubpage() {
+  const [email, setEmail] = useState("");
+
   return (
     <div>
       <h3>Email?</h3>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter your email here..."
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </div>
+      <div>
+        {/* Open the modal using ID.showModal() method */}
+        <button onClick={() => window.modal1.showModal()}>Next</button>
+        <dialog id="modal1">
+          <div>
+            <p>
+              Please click the link we sent to {email} to verify your email
+              address.
+            </p>
+            <div>
+              <form method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button>Back</button>
+              </form>
+              <button>Next</button>
+            </div>
+          </div>
+        </dialog>
+      </div>
     </div>
   );
 }
