3193b2c2278ef584bab9fc8c782f6e0707606f65
Andreas Copan <avcopan@gmail.com>
2023-09-09 13:13:13 -0400

    Resolves #44, updating database.sql

--- database.sql
+++ database.sql
@@ -14,42 +14,25 @@ CREATE INDEX "IDX_session_expire" ON "session" ("expire");
 
 -- For email authentication
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "email" VARCHAR(345) UNIQUE NOT NULL
+  "id" BIGSERIAL PRIMARY KEY,
+  "email" VARCHAR(345) UNIQUE NOT NULL
 );
 
 -- project table init
 CREATE TABLE "project" (
-"id" SERIAL PRIMARY KEY,
-"zip" VARCHAR(10),
-"user_id" INT REFERENCES "user" NOT NULL
+  "id" BIGSERIAL PRIMARY KEY,
+  "zip" VARCHAR(10),
+  "user_id" BIGINT REFERENCES "user" NOT NULL
 );
 
 -- SELECT * FROM "project";
 
--- location table init
-CREATE TABLE "location" (
-"id" SERIAL PRIMARY KEY,
-"level" VARCHAR(80),
-"access" VARCHAR(120)
-);
-
-INSERT INTO "location" (level, access)
-VALUES ('First floor', 'Foot'),
-('First floor', 'Ladder'),
-('Basement', 'Below ground'),
-('Second floor', 'Foot'),
-('Second floor', 'Ladder'),
-('Second floor', 'Roof/deck/ladder');
-
--- SELECT * FROM location;
-
 -- frame table init
 CREATE TABLE "frame" (
-"id" SERIAL PRIMARY KEY,
-"name" VARCHAR(80),
-"description" TEXT,
-"image" TEXT -- this is open for discussion but will probably contain a hash/something that can identify the S3 bucket where we will store the image. can also potentially 
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(80),
+  "description" TEXT,
+  "image" TEXT -- this is open for discussion but will probably contain a hash/something that can identify the S3 bucket where we will store the image. can also potentially 
 );			 -- just be a path to a local image
 
 INSERT INTO "frame" (name, description)
@@ -63,14 +46,12 @@ VALUES ('Casement', 'A casement frame'),
 
 -- window table init
 CREATE TABLE "window" (
-"id" SERIAL PRIMARY KEY,
-"image" TEXT, -- link to stored image in S3 || hash value of image
-"height" INT, -- for both height and width, we can discuss what the proper precision is/if decimals are needed
-"width" INT,
-"location_id" INT REFERENCES "location",
-"current_frame_id" INT REFERENCES "frame",
-"desired_frame_id" INT REFERENCES "frame",
-"project_id" INT REFERENCES "project" NOT NULL
+  "id" BIGSERIAL PRIMARY KEY,
+  "image" TEXT, -- link to stored image in S3 || hash value of image
+  "height" INT, -- for both height and width, we can discuss what the proper precision is/if decimals are needed
+  "width" INT,
+  "desired_frame_id" INT REFERENCES "frame",
+  "project_id" BIGINT REFERENCES "project" NOT NULL
 );
 
 -- SELECT * FROM "window";
