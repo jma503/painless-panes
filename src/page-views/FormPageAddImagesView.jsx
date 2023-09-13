cefb74f4061bc97b9e3dc1f42a20c9779f4dbcd3
Halima Omar <omarhalima100@gmail.com>
2023-09-12 19:59:46 -0500

    resloves #58

--- src/page-views/FormPageAddImagesView.jsx
+++ src/page-views/FormPageAddImagesView.jsx
@@ -6,35 +6,79 @@ import FormPageHeader from "../components/FormPageHeader";
 import FormPageInput from "../components/FormPageInput";
 import FormPageNavigationButtons from "../components/FormPageNavigationButtons";
 import AddWindowImage from "../components/AddImage";
+import Button from "../components/Button";
 
 export default function FormPageAddImages() {
-    const dispatch = useDispatch();
-    const project = useSelector((store) => store.project);
-    const [image, setImage] = useState("");
-    const [imageWidth, setImageWidth] = useState("");
-    const [imageHeight, setImageHeight] = useState("");
+  const dispatch = useDispatch();
+  const project = useSelector((store) => store.project);
+  const [image, setImage] = useState("");
+  const [imageWidth, setImageWidth] = useState("");
+  const [imageHeight, setImageHeight] = useState("");
 
-  
-    return (
-      <>
-        <FormPageHeader text="Take a photo of the window you desire to have replaced" />
-        <AddWindowImage/>
-        <FormPageInput
-          placeholder="Window Width"
-          value={imageWidth}
-          setValue={setImageWidth}
-        />
-        <FormPageInput
-          placeholder="Window Height"
-          value={imageHeight}
-          setValue={setImageHeight}
-        />
-        <FormPageButtonsContainer>
-          <FormPageNavigationButtons
-            page={4}
-          />
-        </FormPageButtonsContainer>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <FormPageHeader text="Take a photo of the window you desire to have replaced" />
+      <AddWindowImage />
+      <FormPageInput
+        placeholder="Window Width"
+        value={imageWidth}
+        setValue={setImageWidth}
+      />
+      <FormPageInput
+        placeholder="Window Height"
+        value={imageHeight}
+        setValue={setImageHeight}
+      />
+      <FormPageButtonsContainer>
+        <FormPageNavigationButtons page={4} />
+      </FormPageButtonsContainer>
+
+      {/* You can open the modal using document.getElementById('ID').showModal() method */}
+      <button
+        className="btn"
+        onClick={() => document.getElementById("my_modal_3").showModal()}
+      >
+      List of Window Frames
+      </button>
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <h3 className="font-bold text-lg">Desired Window Frame</h3>
+          <ul className="py-4">
+            <li>
+              <input type="checkbox" className="checkbox" />
+              <label htmlFor="iamge1"> Casement</label>
+              <img src="/Casement.jpg" alt="Casement" />
+            </li>
+            <li>
+              <input type="checkbox" className="checkbox" />
+              <label htmlFor="iamge2"> Single or Double hung</label>
+              <img src="/Double_Hung.jpg" alt="Single or Double hung frame" />
+            </li>
+            <li>
+              <input type="checkbox" className="checkbox" />
+              <label htmlFor="iamge3"> Egress (basement)</label>
+              <img src="/Casement.jpg" alt="An egress frame" />
+            </li>
+            <li>
+              <input type="checkbox" className="checkbox" />
+              <label htmlFor="image4"> Bay or bow</label>
+              <img src="/Bay.jpg" alt="Bay or bow" />
+            </li>
+            <li>
+              <input type="checkbox" className="checkbox" />
+              <label htmlFor="image5"> Fixed</label>
+              <img src="/Bay.jpg" alt="Fixed" />
+            </li>
+          </ul>
+          <button className="btn btn-primary">Submit</button>
+        </div>
+      </dialog>
+    </>
+  );
+}
