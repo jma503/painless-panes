516ad3efd2b665491aedade8623eadd232db1ff0
Brandon Paulson <brandonpaulson28@gmail.com>
2023-09-13 19:27:38 -0500

    Resolves #74

---
+++ src/page-views/DisplayPriorProjectsView.jsx
@@ -0,0 +1,26 @@
+import {useSelector } from "react-redux";
+import FormPageHeader from "../components/FormPageHeader";
+
+export default function myProjectsView() {
+  const projectWindows = useSelector((store) => store.project);
+
+  return (
+    <>
+      <FormPageHeader text="My Projects" />
+      <div className="overflow-x-auto">
+        {projectWindows.map((project) => {
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img src={project.image} />
+            </figure>
+            <div className="card-body">
+              <p>{project.height}</p>
+              <p>{project.width}</p>
+              <p>{project.desired_frame_id}</p>
+            </div>
+          </div>;
+        })}
+      </div>
+    </>
+  );
+}
