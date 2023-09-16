75fdadd2eee45f64d8234984962c0a1609664f59
Steve Shumaker <steve.j.shumaker@gmail.com>
2023-09-15 20:35:54 -0500

    working edit functionality for h/w

--- src/page-views/FormPageConfirmation.jsx
+++ src/page-views/FormPageConfirmation.jsx
@@ -6,18 +6,47 @@ import FormPageHeader from "../components/FormPageHeader";
 import FormPageButtonsContainer from "../components/FormPageButtonsContainer";
 import FormPageNavigationButtons from "../components/FormPageNavigationButtons";
 import FormPageInput from "../components/FormPageInput";
+import {
+  getAllWindows,
+  updateWindowDimensions,
+} from "../store/sagas/window.saga";
 
 export default function FormPageConfirmation() {
   const dispatch = useDispatch();
   const windows = useSelector((store) => store.allWindows);
   const frameTypes = useSelector((store) => store.frames);
+  const project = useSelector((store) => store.project);
   const [windowToEdit, setWindowToEdit] = useState(null);
+  const [editHeight, setEditHeight] = useState(null);
+  const [editWidth, setEditWidth] = useState(null);
+
+  const editWindow = (window) => {
+    setWindowToEdit(window);
+  };
+
+  const sendEdit = () => {
+    // defines the object to send as the edit
+    const editToSend = {
+      currentWindowId: windowToEdit.id,
+      imageWidth: editWidth,
+      imageHeight: editHeight,
+    };
+    dispatch(updateWindowDimensions(editToSend));
+    // wait one second after the edit is sent - allows the edit to populate
+    // in the database. then, retrieve all windows for the project
+    setTimeout(() => {
+      dispatch(getAllWindows({ project_id: project.id }));
+      setWindowToEdit(null);
+      // should we add a spinner?
+    }, 1000);
+  };
 
   return (
     <>
-      <FormPageHeader text="Confirmation Page" />
       <div className="card w-102 bg-base-100 shadow-xl p-2 m-2">
-        <h2 className="card-title justify-center">Review Submission!</h2>
+        <h2 className="card-title justify-center">
+          Please review your windows:
+        </h2>
         {windows.map((window) => (
           <div
             className="card w-102 bg-base-100 shadow-xl border-solid border-2 p-2 m-2"
@@ -30,15 +59,54 @@ export default function FormPageConfirmation() {
               />
             </figure>
             <div className="card-body items-center text-center">
-              <p>Height: {window.height}</p>
-              <p>Width: {window.width}</p>
+              {!windowToEdit ? (
+                <p>Height: {window.height}</p>
+              ) : (
+                <FormPageInput
+                  value={editHeight}
+                  placeholder={`Height: ${window.height}`}
+                  setValue={setEditHeight}
+                />
+              )}
+              {!windowToEdit ? (
+                <p>Width: {window.width}</p>
+              ) : (
+                <FormPageInput
+                  value={editWidth}
+                  placeholder={`Width: ${window.width}`}
+                  setValue={setEditWidth}
+                />
+              )}
               <p>Desired frame: {frameTypes[window.desired_frame_id].name}</p>
             </div>
+            {!windowToEdit ? (
+              <div className="card-actions justify-end">
+                <button
+                  className="btn btn-sm"
+                  onClick={() => {
+                    editWindow(window);
+                  }}
+                >
+                  edit
+                </button>
+              </div>
+            ) : (
+              <div className="card-actions justify-end">
+                <button
+                  className="btn btn-sm"
+                  onClick={() => {
+                    sendEdit();
+                  }}
+                >
+                  save
+                </button>
+              </div>
+            )}
           </div>
         ))}
         <div className="card-body">
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Edit</button>
+          <div className="card-actions justify-center">
+            {/* <button className="btn btn-primary">Edit</button> */}
             <button className="btn btn-primary">Submit</button>
           </div>
         </div>
