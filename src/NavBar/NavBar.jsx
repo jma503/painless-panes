e07a93bd4179108104c8c387c954f637f52b078a
Laokeng Vang <laokengv@gmail.com>
2023-09-19 20:04:19 -0500

    NavBar is working

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,12 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
-  <div className="navbar bg-base-100">
-    <Link className="btn btn-ghost normal-case text-xl" to="/formPageAddImagesView">
-      Get Windows
-    </Link>
-    <Link className="btn btn-ghost normal-case text-xl" to="/priorProjectsPage">
-      Prior Projects
-    </Link>
-  </div>
+
+    return (
+        <div className="navbar bg-base-100">
+            <Link className="btn btn-ghost normal-case text-xl" to="/" >
+                Get Windows
+            </Link>
+
+            <Link className="btn btn-ghost normal-case text-xl" to="/projects" >
+                My Projects
+            </Link>
+        </div>
+    )
 };
